98b1ce2e80cfca6f3886d2a226811103a299e5aa
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2025-02-04 13:36:36 -0700

    chore: organize store, related scripts

--- packages/store/src/database.ts
+++ packages/store/src/database.ts
@@ -1,55 +1,21 @@
 import { config } from "@qnaplus/config";
-import { Question, getAllQuestions as archiverGetAllQuestions, fetchCurrentSeason, getOldestQuestion, getOldestUnansweredQuestion } from "@qnaplus/scraper";
+import { Question } from "@qnaplus/scraper";
 import { createClient } from "@supabase/supabase-js";
-import { and, eq, gte, inArray } from "drizzle-orm";
+import { and, eq, gte, inArray, sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/postgres-js";
-import { Logger } from "pino";
-import { ChangeQuestion, classifyChanges } from "./change_classifier";
-import { PayloadQueue, RenotifyPayload, UpdatePayload } from "./payload_queue";
-import { QnaplusChannels, QnaplusEvents } from "./resources";
 import * as schema from "./schema";
 import { trycatch } from "./trycatch";
 
 export const supabase = createClient(config.getenv("SUPABASE_URL"), config.getenv("SUPABASE_KEY"))
-const db = drizzle({
+export const db = drizzle({
     schema,
     connection: config.getenv("SUPABASE_CONNECTION_STRING")
 });
 
-const METADATA_ROW_ID = 0;
+export const METADATA_ROW_ID = 0;
 
-export const populate = async (logger?: Logger) => {
-    const { questions } = await archiverGetAllQuestions({ logger, trySessionRefresh: true });
-    return insertQuestions(questions);
-}
-
-export const populateWithMetadata = async (logger?: Logger) => {
-    const { questions } = await archiverGetAllQuestions({ logger, trySessionRefresh: true });
-    const currentSeason = await fetchCurrentSeason({ logger, trySessionRefresh: true });
-
-    const oldestUnansweredQuestion = getOldestUnansweredQuestion(questions, currentSeason);
-    const oldestQuestion = getOldestQuestion(questions, currentSeason);
-
-    // assert non-null since we know the scraper is starting from the beginning
-    // meaning we are practically guaranteed at least one "oldest question" 
-    const oldestQuestionId = oldestUnansweredQuestion !== undefined
-        ? oldestUnansweredQuestion.id
-        : oldestQuestion!.id;
-
-    const { ok, error } = await trycatch(db.transaction(async tx => {
-        await tx
-            .insert(schema.questions)
-            .values(questions);
-        await tx
-            .insert(schema.metadata)
-            .values({ id: METADATA_ROW_ID, currentSeason, oldestUnansweredQuestion: oldestQuestionId })
-            .onConflictDoNothing();
-    }));
-    if (!ok) {
-        logger?.error({ error }, "An error occurred while attempting to populate the database.");
-        return;
-    }
-    logger?.info("Successfully populated database");
+export const testConnection = async () => {
+    return trycatch(db.execute(sql`select 1`));
 }
 
 export const getQuestion = async (id: Question["id"]) => {
@@ -118,49 +84,4 @@ export const clearRenotifyQueue = async () => {
 
 export const insertRenotifyQueue = async (ids: { id: string }[]) => {
     return trycatch(db.insert(schema.renotify_queue).values(ids).onConflictDoNothing());
-}
-
-export type ChangeCallback = (items: ChangeQuestion[]) => void | Promise<void>;
-
-export const onChange = (callback: ChangeCallback, logger?: Logger) => {
-    const queue = new PayloadQueue<UpdatePayload<Question>>({
-        onFlush(items) {
-            const changes = classifyChanges(items);
-            if (changes.length < 1) {
-                logger?.info("No changes detected.");
-                return;
-            }
-            logger?.info(`${changes.length} changes detected.`);
-            callback(changes);
-        }
-    });
-    return supabase
-        .channel(QnaplusChannels.DbChanges)
-        .on<Question>(
-            "postgres_changes",
-            {
-                event: "UPDATE",
-                schema: "public",
-                table: schema.questions._.name,
-            },
-            payload => queue.push({ old: payload.old, new: payload.new })
-        )
-        .on<RenotifyPayload>(
-            "broadcast",
-            { event: QnaplusEvents.RenotifyQueueFlush },
-            async ({ payload }) => {
-                const { questions } = payload;
-                const items = questions.map<UpdatePayload<Question>>(p => ({ old: { ...p, answered: false }, new: p }));
-                queue.push(...items);
-                const result = await supabase
-                    .channel(QnaplusChannels.RenotifyQueue)
-                    .send({
-                        type: "broadcast",
-                        event: QnaplusEvents.RenotifyQueueFlushAck,
-                        payload: {}
-                    });
-                logger?.info(`Sent renotify queue acknowledgement with result '${result}'`);
-            }
-        )
-        .subscribe();
 }
