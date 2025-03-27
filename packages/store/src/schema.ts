cf3b374e7c0ad88d59c87e1e13a9f67b7ce78a44
Battlesquid <esanoluwamayowa@gmail.com>
2025-03-27 10:18:35 -0700

    chore: run neat

--- packages/store/src/schema.ts
+++ packages/store/src/schema.ts
@@ -7,8 +7,8 @@ import {
 	jsonb,
 	pgTable,
 	text,
+	timestamp,
 	uuid,
-    timestamp,
 } from "drizzle-orm/pg-core";
 import { EventQueueType } from "./schema_types";
 
@@ -44,7 +44,7 @@ export const failures = pgTable("failures", {
 export const event_queue = pgTable("event_queue", {
 	id: uuid().defaultRandom().primaryKey(),
 	event: text({ enum: Object.values(EventQueueType) as [string] }).notNull(),
-    timestamp: timestamp().notNull().defaultNow(),
+	timestamp: timestamp().notNull().defaultNow(),
 	payload: jsonb().notNull(),
 }).enableRLS();
 
