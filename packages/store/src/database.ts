d531fb0a11a7acf1795a8d4e827f6de198ef781e
Battlesquid <esanoluwamayowa@gmail.com>
2025-03-28 22:30:59 -0700

    fix: fix event queue aggregation

--- packages/store/src/database.ts
+++ packages/store/src/database.ts
@@ -11,251 +11,269 @@ const pg = lazy(() => postgres(getenv("SUPABASE_TRANSACTION_URL")));
 export const db = lazy(() => drizzle({ schema, client: pg() }));
 
 export const disconnectPgClient = async () => {
-	const client = pg();
-	if (client !== null) {
-		await client.end();
-	}
+    const client = pg();
+    if (client !== null) {
+        await client.end();
+    }
 };
 
 export const METADATA_ROW_ID = 0;
 
 export const testConnection = async (
-	d: PostgresJsDatabase<typeof schema> = db(),
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(d.execute(sql`select 1`));
+    return trycatch(d.execute(sql`select 1`));
 };
 
 export const getQuestion = async (
-	id: Question["id"],
-	d: PostgresJsDatabase<typeof schema> = db(),
+    id: Question["id"],
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(
-		d.query.questions.findFirst({ where: eq(schema.questions.id, id) }),
-	);
+    return trycatch(
+        d.query.questions.findFirst({ where: eq(schema.questions.id, id) }),
+    );
 };
 
 export const getAllQuestions = async () => {
-	return trycatch(db().select().from(schema.questions));
+    return trycatch(db().select().from(schema.questions));
 };
 
 export const getAnsweredQuestionsNewerThanDate = async (
-	ms: number,
-	d: PostgresJsDatabase<typeof schema> = db(),
+    ms: number,
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(
-		d
-			.select()
-			.from(schema.questions)
-			.where(
-				and(
-					gte(schema.questions.answeredTimestampMs, ms),
-					eq(schema.questions.answered, true),
-				),
-			),
-	);
+    return trycatch(
+        d
+            .select()
+            .from(schema.questions)
+            .where(
+                and(
+                    gte(schema.questions.answeredTimestampMs, ms),
+                    eq(schema.questions.answered, true),
+                ),
+            ),
+    );
 };
 
 export const insertQuestions = async (
-	data: Question[],
-	d: PostgresJsDatabase<typeof schema> = db(),
+    data: Question[],
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(d.insert(schema.questions).values(data));
+    return trycatch(d.insert(schema.questions).values(data));
 };
 
 const EXCLUDED_QUESTION = {
-	id: sql`excluded.id`,
-	url: sql`excluded.url`,
-	author: sql`excluded.author`,
-	program: sql`excluded.program`,
-	title: sql`excluded.title`,
-	question: sql`excluded.question`,
-	questionRaw: sql`excluded."questionRaw"`,
-	answer: sql`excluded.answer`,
-	answerRaw: sql`excluded."answerRaw"`,
-	season: sql`excluded.season`,
-	askedTimestamp: sql`excluded."askedTimestamp"`,
-	askedTimestampMs: sql`excluded."askedTimestampMs"`,
-	answeredTimestamp: sql`excluded."answeredTimestamp"`,
-	answeredTimestampMs: sql`excluded."answeredTimestampMs"`,
-	answered: sql`excluded.answered`,
-	tags: sql`excluded.tags`,
+    id: sql`excluded.id`,
+    url: sql`excluded.url`,
+    author: sql`excluded.author`,
+    program: sql`excluded.program`,
+    title: sql`excluded.title`,
+    question: sql`excluded.question`,
+    questionRaw: sql`excluded."questionRaw"`,
+    answer: sql`excluded.answer`,
+    answerRaw: sql`excluded."answerRaw"`,
+    season: sql`excluded.season`,
+    askedTimestamp: sql`excluded."askedTimestamp"`,
+    askedTimestampMs: sql`excluded."askedTimestampMs"`,
+    answeredTimestamp: sql`excluded."answeredTimestamp"`,
+    answeredTimestampMs: sql`excluded."answeredTimestampMs"`,
+    answered: sql`excluded.answered`,
+    tags: sql`excluded.tags`,
 };
 
 const QUESTION_UPDATED_QUERY = or(
-	sql`${schema.questions.question} != excluded.question`,
-	sql`${schema.questions.answer} IS DISTINCT FROM excluded.answer`,
-	sql`${schema.questions.answered} != excluded.answered`,
+    sql`${schema.questions.question} != excluded.question`,
+    sql`${schema.questions.answer} IS DISTINCT FROM excluded.answer`,
+    sql`${schema.questions.answered} != excluded.answered`,
 );
 
 export const updateQuestions = async (
-	data: Question[],
-	d: PostgresJsDatabase<typeof schema> = db(),
+    data: Question[],
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(
-		d
-			.insert(schema.questions)
-			.values(data)
-			.onConflictDoUpdate({
-				target: schema.questions.id,
-				set: EXCLUDED_QUESTION,
-				setWhere: QUESTION_UPDATED_QUERY,
-			})
-			.returning(),
-	);
+    return trycatch(
+        d
+            .insert(schema.questions)
+            .values(data)
+            .onConflictDoUpdate({
+                target: schema.questions.id,
+                set: EXCLUDED_QUESTION,
+                setWhere: QUESTION_UPDATED_QUERY,
+            })
+            .returning(),
+    );
 };
 
 export const getEventQueue = async (
-	d: PostgresJsDatabase<typeof schema> = db(),
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	const formattedPayloads = d.$with("formatted_payloads").as(
-		d
-			.select({
-				event: schema.event_queue.event,
-				payload: sql`jsonb_build_object('id', ${schema.event_queue.id}, 'payload', ${schema.event_queue.payload})`,
-			})
-			.from(schema.event_queue),
-	);
-	const aggregatedPayloads = d.$with("aggregated_payloads").as(
-		d
-			.select({
-				event: schema.event_queue.event,
-				payloads: sql`array_agg(${schema.event_queue.payload})`.as("payloads"),
-			})
-			.from(formattedPayloads)
-			.groupBy(schema.event_queue.event),
-	);
-	return trycatch(
-		d
-			.with(aggregatedPayloads)
-			.select({
-				queue: sql<EventQueueAggregation>`jsonb_object_agg(${schema.event_queue.event}, ${aggregatedPayloads.payloads})`,
-			})
-			.from(aggregatedPayloads),
-	);
+    // return trycatch(
+    //     d.execute(
+    //         sql<EventQueueAggregation>`
+    //         WITH formatted_payloads AS (
+    //             SELECT event, jsonb_build_object('id', id, 'payload', payload) AS payload
+    //             FROM event_queue
+    //         ),
+    //         aggregated_payloads AS (
+    //             SELECT event, array_agg(payload) AS payloads
+    //             FROM formatted_payloads e
+    //             GROUP BY e.event
+    //         )
+    //         SELECT jsonb_object_agg(event, payloads) AS event_payloads
+    //         FROM aggregated_payloads;
+    //     `
+    //     )
+    // );
+    const formattedPayloads = d.$with("formatted_payloads").as(
+        d
+            .select({
+                event: schema.event_queue.event,
+                payload: sql`jsonb_build_object('id', ${schema.event_queue.id}, 'payload', ${schema.event_queue.payload})`.as("payload"),
+            })
+            .from(schema.event_queue),
+    );
+
+    const aggregatedPayloads = d.$with("aggregated_payloads").as(
+        d
+            .select({
+                event: formattedPayloads.event,
+                payloads: sql`array_agg(${formattedPayloads.payload})`.as("payloads"),
+            })
+            .from(formattedPayloads)
+            .groupBy(formattedPayloads.event),
+    );
+    return trycatch(
+        d
+            .with(formattedPayloads, aggregatedPayloads)
+            .select({
+                queue: sql<EventQueueAggregation | null>`jsonb_object_agg(${schema.event_queue.event}, ${aggregatedPayloads.payloads})`,
+            })
+            .from(aggregatedPayloads)
+    );
 };
 
 export const clearEventQueue = async (
-	ids: string[],
-	d: PostgresJsDatabase<typeof schema> = db(),
+    ids: string[],
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(
-		d.delete(schema.event_queue).where(inArray(schema.event_queue.id, ids)),
-	);
+    return trycatch(
+        d.delete(schema.event_queue).where(inArray(schema.event_queue.id, ids)),
+    );
 };
 
 export const getMetadata = async (
-	d: PostgresJsDatabase<typeof schema> = db(),
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(d.query.metadata.findFirst());
+    return trycatch(d.query.metadata.findFirst());
 };
 
 export const updateMetadata = async (
-	data: Partial<Omit<typeof schema.metadata.$inferInsert, "id">>,
-	d: PostgresJsDatabase<typeof schema> = db(),
+    data: Partial<Omit<typeof schema.metadata.$inferInsert, "id">>,
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(
-		d
-			.update(schema.metadata)
-			.set({ ...data })
-			.where(eq(schema.metadata.id, METADATA_ROW_ID)),
-	);
+    return trycatch(
+        d
+            .update(schema.metadata)
+            .set({ ...data })
+            .where(eq(schema.metadata.id, METADATA_ROW_ID)),
+    );
 };
 
 export const getFailures = async (
-	d: PostgresJsDatabase<typeof schema> = db(),
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(d.select().from(schema.failures));
+    return trycatch(d.select().from(schema.failures));
 };
 
 export const updateFailures = async (
-	data: (typeof schema.failures.$inferInsert)[],
-	d: PostgresJsDatabase<typeof schema> = db(),
+    data: (typeof schema.failures.$inferInsert)[],
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(d.insert(schema.failures).values(data).onConflictDoNothing());
+    return trycatch(d.insert(schema.failures).values(data).onConflictDoNothing());
 };
 
 export const doFailureQuestionUpdate = async (
-	questions: Question[],
-	d: PostgresJsDatabase<typeof schema> = db(),
+    questions: Question[],
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	const oldFailures = questions.map((q) => q.id);
-	return trycatch(
-		d.transaction(async (tx) => {
-			await updateQuestions(questions, tx);
-			await tx
-				.delete(schema.failures)
-				.where(inArray(schema.failures.id, oldFailures));
-		}),
-	);
+    const oldFailures = questions.map((q) => q.id);
+    return trycatch(
+        d.transaction(async (tx) => {
+            await updateQuestions(questions, tx);
+            await tx
+                .delete(schema.failures)
+                .where(inArray(schema.failures.id, oldFailures));
+        }),
+    );
 };
 
 export const getReplayEvents = async (
-	d: PostgresJsDatabase<typeof schema> = db(),
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(
-		d
-			.select({ question: schema.questions })
-			.from(schema.event_queue)
-			.innerJoin(
-				schema.questions,
-				eq(schema.event_queue.event, EventQueueType.Replay),
-			),
-	);
+    return trycatch(
+        d
+            .select({ question: schema.questions })
+            .from(schema.event_queue)
+            .innerJoin(
+                schema.questions,
+                eq(schema.event_queue.event, EventQueueType.Replay),
+            ),
+    );
 };
 
 export const insertReplayEvents = async (
-	questions: Question[],
-	d: PostgresJsDatabase<typeof schema> = db(),
+    questions: Question[],
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	const events = questions.map((question) => ({
-		event: EventQueueType.Replay,
-		payload: { question },
-	}));
-	return trycatch(
-		d.insert(schema.event_queue).values(events).onConflictDoNothing(),
-	);
+    const events = questions.map((question) => ({
+        event: EventQueueType.Replay,
+        payload: { question },
+    }));
+    return trycatch(
+        d.insert(schema.event_queue).values(events).onConflictDoNothing(),
+    );
 };
 
 export const clearReplayEvents = async (
-	d: PostgresJsDatabase<typeof schema> = db(),
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(
-		d
-			.delete(schema.event_queue)
-			.where(eq(schema.event_queue.event, EventQueueType.Replay)),
-	);
+    return trycatch(
+        d
+            .delete(schema.event_queue)
+            .where(eq(schema.event_queue.event, EventQueueType.Replay)),
+    );
 };
 
 export const getAllPrograms = async (
-	d: PostgresJsDatabase<typeof schema> = db(),
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(
-		d
-			.selectDistinct({ program: schema.questions.program })
-			.from(schema.questions),
-	);
+    return trycatch(
+        d
+            .selectDistinct({ program: schema.questions.program })
+            .from(schema.questions),
+    );
 };
 
 export const getForumStates = async (
-	d: PostgresJsDatabase<typeof schema> = db(),
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(d.select().from(schema.forum_state));
+    return trycatch(d.select().from(schema.forum_state));
 };
 
 export const updateForumStates = async (
-	states: (typeof schema.forum_state.$inferInsert)[],
-	d: PostgresJsDatabase<typeof schema> = db(),
+    states: (typeof schema.forum_state.$inferInsert)[],
+    d: PostgresJsDatabase<typeof schema> = db(),
 ) => {
-	return trycatch(
-		d
-			.insert(schema.forum_state)
-			.values(states)
-			.onConflictDoUpdate({
-				target: schema.forum_state.program,
-				set: {
-					open: sql`excluded.open`,
-				},
-				setWhere: sql`${schema.forum_state.open} != excluded.open`,
-			}),
-	);
+    return trycatch(
+        d
+            .insert(schema.forum_state)
+            .values(states)
+            .onConflictDoUpdate({
+                target: schema.forum_state.program,
+                set: {
+                    open: sql`excluded.open`,
+                },
+                setWhere: sql`${schema.forum_state.open} != excluded.open`,
+            }),
+    );
 };
